3e4346787a02d0a333b0e159f5c1cc7d700222fa
RootKarka <patrick.pajares@tecsup.edu.pe>
2026-06-28 17:46:02 -0500

    Correcion del DiagnosticoFragment

--- app/src/main/java/com/tecsup/aresapp/feature/diagnostico/DiagnosticoFragment.kt
+++ app/src/main/java/com/tecsup/aresapp/feature/diagnostico/DiagnosticoFragment.kt
@@ -1,4 +1,4 @@
-package com.tecsup.aresapp
+package com.tecsup.aresapp.feature.diagnostico
 
 import android.os.Bundle
 import android.view.LayoutInflater
@@ -7,6 +7,7 @@ import android.view.ViewGroup
 import androidx.fragment.app.Fragment
 import com.google.android.material.button.MaterialButton
 import com.google.android.material.card.MaterialCardView
+import com.tecsup.aresapp.R
 
 class Diagnostico : Fragment() {
 
